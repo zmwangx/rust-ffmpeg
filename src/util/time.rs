03b1d25fde29981298033fe2659ddb67f342c12e
meh <meh@schizofreni.co>
2015-08-05 19:34:43 +0200

    time: add inline attributes

--- src/util/time.rs
+++ src/util/time.rs
@@ -1,24 +1,28 @@
 use ffi::*;
 use ::Error;
 
+#[inline(always)]
 pub fn current() -> i64 {
 	unsafe {
 		av_gettime() as i64
 	}
 }
 
+#[inline(always)]
 pub fn relative() -> i64 {
 	unsafe {
 		av_gettime_relative() as i64
 	}
 }
 
+#[inline(always)]
 pub fn is_monotonic() -> bool {
 	unsafe {
 		av_gettime_relative_is_monotonic() != 0
 	}
 }
 
+#[inline(always)]
 pub fn sleep(usec: u32) -> Result<(), Error> {
 	unsafe {
 		match av_usleep(usec) {

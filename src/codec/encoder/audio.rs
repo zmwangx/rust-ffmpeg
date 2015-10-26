d2b6557fbce8822aedc854c7040271b2c832c331
meh <meh@schizofreni.co>
2015-10-26 20:27:04 +0100

    codec/encoder/audio: add open_with() constructor

--- src/codec/encoder/audio.rs
+++ src/codec/encoder/audio.rs
@@ -35,6 +35,20 @@ impl Audio {
 		}
 	}
 
+	pub fn open_with(mut self, options: Dictionary) -> Result<Encoder, Error> {
+		unsafe {
+			let mut opts = options.disown();
+			let     res  = avcodec_open2(self.as_mut_ptr(), ptr::null(), &mut opts);
+
+			Dictionary::own(opts);
+
+			match res {
+				0 => Ok(Encoder(self)),
+				e => Err(Error::from(e))
+			}
+		}
+	}
+
 	pub fn open_as_with<E: traits::Encoder>(mut self, codec: E, options: Dictionary) -> Result<Encoder, Error> {
 		unsafe {
 			if let Some(codec) = codec.encoder() {

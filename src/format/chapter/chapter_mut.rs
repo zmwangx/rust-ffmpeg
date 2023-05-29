91b7f7a6644704cec3333ab41de5cabdeb621587
Kian-Meng Ang <kianmeng@cpan.org>
2023-05-29 23:09:44 +0800

    Fix typos
    Found via `typos --format brief`

--- src/format/chapter/chapter_mut.rs
+++ src/format/chapter/chapter_mut.rs
@@ -7,7 +7,7 @@ use format::context::common::Context;
 use {Dictionary, DictionaryMut, Rational};
 
 // WARNING: index refers to the offset in the chapters array (starting from 0)
-// it is not necessarly equal to the id (which may start at 1)
+// it is not necessarily equal to the id (which may start at 1)
 pub struct ChapterMut<'a> {
     context: &'a mut Context,
     index: usize,

40ea5fdfb43a40cf9bced9728de5e94e58f59b2e
Polochon-street <polochonstreet@gmx.fr>
2024-07-28 00:37:13 +0200

    Fix CI... (#202)
    ...Because of https://blog.rust-lang.org/2024/05/06/check-cfg.html

--- build.rs
+++ build.rs
@@ -1,10 +1,16 @@
 use std::env;
 
 fn main() {
-    for (name, _value) in env::vars() {
+    for (name, value) in env::vars() {
         if name.starts_with("DEP_FFMPEG_") {
+            if value == "true" {
+                println!(
+                    r#"cargo:rustc-cfg=feature="{}""#,
+                    name["DEP_FFMPEG_".len()..name.len()].to_lowercase()
+                );
+            }
             println!(
-                r#"cargo:rustc-cfg=feature="{}""#,
+                r#"cargo:rustc-check-cfg=cfg(feature, values("{}"))"#,
                 name["DEP_FFMPEG_".len()..name.len()].to_lowercase()
             );
         }
